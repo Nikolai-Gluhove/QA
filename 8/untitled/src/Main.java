533814f936aef18d921dfc11b77dc04d79d5f951
Nikolai-Gluhove <106698326+Nikolai-Gluhove@users.noreply.github.com>
2023-04-26 11:49:20 +0300

    Start QA tutorial

---
+++ 8/untitled/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+
+        for(int i = 0; i < array.length; i++){
+            array[i] = i + 1;
+        }
+
+        Calculate calculate = new Calculate();
+        int[] result = calculate.calculate(array, 5);
+
+        for(int i = 0; i < array.length; i++){
+            if(array[i] % 2 == 0){
+                System.out.println(result[i]);
+            }
+        }
+
+
+
+    }
+}
