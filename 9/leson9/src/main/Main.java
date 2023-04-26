8f40974e3ac5f15ef155f43ae37367903340dff5
Nikolai-Gluhove <106698326+Nikolai-Gluhove@users.noreply.github.com>
2023-04-26 13:42:40 +0300

    lesson 9 and correct 8

---
+++ 9/leson9/src/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Тип char
+//        char c = '2';
+//        char c1 = 50;
+//        String s = "22";
+
+
+        // Целочисленные типы
+//        int a = 10;
+//        int b = 5;
+//
+//        System.out.println(a++ + b);
+//        System.out.println(a);
+
+
+        // Булевая логика
+//        int a = 10;
+//        int b = 5;
+//        boolean f = a == 10;
+//        System.out.println(f);
+//        boolean c = (a != 10) && (b == 5);
+//        System.out.println(c);
+//        System.out.println(!f);
+//        System.out.println(!c);
+
+
+        // Строки
+        String s = "Hello world dff dsaf dsfds sd sa dffw d";
+
+        String[] e = s.split(" ");
+        System.out.println(Arrays.toString(e));
+
+
+    }
+}
