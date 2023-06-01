1839463a22696537b81507d27eedaa217f79ddb1
Nikolai-Gluhove <106698326+Nikolai-Gluhove@users.noreply.github.com>
2023-06-01 14:18:39 +0300

    start lesson_14

--- 13/untitled/src/main/java/org/example/data/Command.java
+++
@@ -1,7 +0,0 @@
-package org.example.data;
-
-public enum Command {
-    ADD,
-    LIST,
-    EXIT
-}
--- 13/untitled/src/main/java/org/example/data/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.example.data;
-
-//константы (не путать с enom)
-public class Constants {
-    private static final String MOSCOW = "Москва";
-    private static final String Vashinkton = "Вашинктон";
-}
